8dc6577c1b5bfebe797ddbee206318c4383a8386
CharlesC <charlesclayton629@gmail.com>
2024-02-11 01:10:04 +0000

    css for front screen using a sass package. css is mostly complete.

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -2,6 +2,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
 import { Register } from './Pages/Register';
+import "./style.scss"
 
 function App() {
 
--- chat-app/src/Pages/Register.jsx
+++ chat-app/src/Pages/Register.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
+//import "./style.scss"
 
-const Register = () => {
+export const Register = () => {
   return (
     <div className='formContainer'>
         <div className='formWrapper'>
             <span className = "logo">Lama </span>
+            <span className = 'title'>Register</span>
             <form>
                 <input type="text" placeholder='display name'/>
                 <input type="email" placeholder='email'/>
                 <input type="password" placeholder='password'/>
                 <input type="file" />
+                <button>Sign Up</button>
             </form>
+            <p>Do you have an account? Login</p>
         </div>
     </div>
   )
 }
-
-export default Register; //export this file
