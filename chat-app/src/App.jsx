c52907463a9d88eaf2d4025ac91a9a600080783b
CharlesC <charlesclayton629@gmail.com>
2024-02-28 02:18:04 +0000

    Firestore addition, fixed bugs

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -8,7 +8,7 @@ import {Home} from './Pages/Home';
 function App() {
 
   return (
-    <Home/>
+    <Register/>
   );
 }
 
--- chat-app/src/Pages/Register.jsx
+++ chat-app/src/Pages/Register.jsx
@@ -1,12 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
 import Add from "../img/addAvatar.png"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+
 export const Register = () => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value)
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+
+const res = await createUserWithEmailAndPassword(auth, email, password); //user
+const storageRef = ref(storage, displayName);
+
+const uploadTask = uploadBytesResumable(storageRef, file);
+
+// Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+uploadTask.on(
+  (error) => {
+    setErr(true)
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+    });
+  }
+);
+
+    }catch(err){
+      setErr(true)
+    }
+
+}
+
   return (
     <div className='formContainer'>
         <div className='formWrapper'>
             <span className = "logo">Chat App </span>
             <span className = 'title'>Register</span>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder='Username'/>
                 <input type="email" placeholder='Email'/>
                 <input type="password" placeholder='Password'/>
@@ -16,6 +72,7 @@ export const Register = () => {
                   <button type="button">Change Avatar</button>
                 </label>
                 <button>Sign Up</button>
+                {err && <span>Something went wrong</span>}
             </form>
             <p>Do you have an account? Login</p>
         </div>
